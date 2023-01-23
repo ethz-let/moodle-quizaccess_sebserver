4d5dd39484cd2babf29f1c746a951ffc07ca0c3a
Nexterday <nexterday@users.noreply.github.com>
2023-01-23 18:03:30 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023011101;
+$plugin->version   = 2023012200;
 $plugin->requires  = 2019111809;
 $plugin->cron      = 0;
 $plugin->component = 'quizaccess_sebserver';
