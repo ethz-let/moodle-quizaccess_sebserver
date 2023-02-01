8b97eba7a54ee180275a5fd2e69571013c71b783
Nexterday <nexterday@users.noreply.github.com>
2023-02-01 08:53:10 +0100

    Add files via upload

---
+++ lang/de/quizaccess_sebserver.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for the quizaccess_sebserver plugin.
+ *
+ * @package   quizaccess_sebserver
+ * @author    Amr Hourani (amr.hourani@let.ethz.ch)
+ * @copyright 2022 ETH Zurich
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'SEB Server';
+$string['quizismanagedbysebserver'] = 'Die Prüfung wird von SEB Server verwaltet. Sie müssen den Safe Exam Browser starten, bevor Sie an der Prüfung teilnehmen.';
+$string['managedbysebserver'] = 'Die Prüfung wird von SEB Server verwaltet. Das Ändern von Werten kann sich auf die Untersuchungseinstellungen auswirken, bitte gehen Sie vorsichtig vor. Sie können die Verbindung zum SEB-Server über den Abschnitt SEB-Server unten deaktivieren';
+$string['enablesebserver'] = 'SEB-Server aktivieren';
+$string['sebserver:managesebserver'] = 'SEB-Server verwalten';
+$string['privacy:metadata'] = 'Das SEB-Server-Plugin speichert keine personenbezogenen Daten.';
