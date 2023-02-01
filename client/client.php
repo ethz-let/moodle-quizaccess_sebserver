a3de4f70888471e4acf5bbaeea7f1731ed666ef8
Nexterday <nexterday@users.noreply.github.com>
2023-02-01 08:58:10 +0100

    Update client.php

--- client/client.php
+++ client/client.php
@@ -11,35 +11,19 @@
  * This script does not depend of any Moodle code,
  * and it can be called from a browser.
  *
- * @author Amr Hourani
  */
-
-/// MOODLE ADMINISTRATION SETUP STEPS
-// 1- Install the plugin
-// 2- Enable web service advance feature (Admin > Advanced features)
-// 3- Enable preferred web service protocols (Admin > Plugins > Web services > Manage protocols)
-// 4- Create a token for a specific user (or at site level if wanted; i.e admin user) and for the service 'SEB Authentication Key Web Serice' (Admin > Plugins > Web services > Manage tokens)
-// 5- Endpoint will be dependant on the used protocol: MOODLE_DOMAIN/webservice/PROTOCOL/server.php
-//    ie: Endpoint for xmlrpc:  MOODLE_DOMAIN/webservice/xmlrpc/server.php
-//    ie: Endpoint for soap:  MOODLE_DOMAIN/webservice/soap/server.php
-//    ie: Endpoint for rest:  MOODLE_DOMAIN/webservice/rest/server.php
 
 // !!WARNING!!: you should never allow the token to be non-expired. Please expire the tokens in moodle to as soon as the exam finishes.
 
-// This example is based on xmlrpc
+// This example is based on xmlrpc.
 
 /// SETUP - NEED TO BE CHANGED
 
 $token = '076ff9e55fdcba2dccab1e6706d4bac6';
-$domainname = 'http://localhost/311';
+$domainname = 'http://localhost/';
 
 /// FUNCTION NAME
-//$functionname = 'quizaccess_sebserver_backup_course';
 $functionname = 'quizaccess_sebserver_get_exams';
-// http://localhost/311/mod/quiz/accessrule/sebserver/classes/external/client/client.php
-
-/// PARAMETERS
-//$id = 2;
 
 ///// XML-RPC CALL
 header('Content-Type: text/plain');
@@ -47,13 +31,8 @@ $serverurl = $domainname . '/webservice/xmlrpc/server.php'. '?wstoken=' . $token
 
 require_once('./curl.php');
 
-//array(array(1,2,3),array(4,5),8,'lol')
-// array(8)
 $curl = new curl;
 $post = xmlrpc_encode_request($functionname, array(array(2))  , array ('encoding' => 'utf-8'));
 $resp = xmlrpc_decode($curl->post($serverurl, $post));
 
 print_r($resp);
-//echo json_encode($resp);
-//echo json_last_error();
-echo "Done..";
