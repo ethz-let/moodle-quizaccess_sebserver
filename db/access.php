55926bd60b14b67d61d97be95070649f5db70a69
Nexterday <nexterday@users.noreply.github.com>
2023-05-09 15:40:34 +0200

    Update access.php

--- db/access.php
+++ db/access.php
@@ -17,7 +17,7 @@
 /**
  * Define capabilities for plugin.
  *
- * @package    quizaccess_seberver
+ * @package    quizaccess_sebserver
  * @author     Amr Hourani <amr.hourani@let.eth.ch>
  * @copyright  2023 ETH LET
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
