282465c145182afa822a3c30addcc749ebef0a87
isacsonk <7068702+isacsonk@users.noreply.github.com>
2026-06-30 16:12:36 +0200

    Update quizaccess_sebserver.php

--- lang/de/quizaccess_sebserver.php
+++ lang/de/quizaccess_sebserver.php
@@ -85,8 +85,8 @@ $string['invalidversiontitle'] = 'Falsche SEB Version';
 $string['invalidversionmsg'] = 'Die installierte Version des Safe Exam Browser erfüllt nicht die festgelegten Anforderungen. Bitte installieren Sie die erforderliche Version und starten Sie den Safe Exam Browser erneut.';
 $string['currentversion'] = 'Installierte SEB Client Version';
 $string['requiredversions'] = 'Erforderliche SEB Client Version(en)';
-$string['howtofixversionerror'] = '<b>Um dies zu beheben:</b><ol><li>Klicken Sie den Button Safe Exam Browser beenden um SEB zu beenden.</li>
-<li>Klicken Sie den Button Safe Exam Browser downloaden und installieren Sie, die für Ihr Betriebssystem erforderliche (Mindest-)Version des Safe Exam Browsers.</li>
+$string['howtofixversionerror'] = '<b>Um dies zu beheben:</b><ol><li>Klicken Sie den Button "Safe Exam Browser beenden" um SEB zu beenden.</li>
+<li>Klicken Sie den Button "Safe Exam Browser downloaden" und installieren Sie, die für Ihr Betriebssystem erforderliche (Mindest-)Version des Safe Exam Browsers.</li>
 <li>Starten Sie Ihren Computer neu und gehen Sie wieder zum Quiz.</li></ol>';
 $string['checkingversion'] = 'Überprüfe SEB Client Version..';
 $string['allowedsebversions'] = 'Erlaubte SEB Client Version(en)';
