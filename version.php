d601263fe37392b81c21261cd25c515a29dc7911
isacsonk <7068702+isacsonk@users.noreply.github.com>
2026-06-30 16:50:22 +0200

    Update version.php
    updated version to 2026063000

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026062900;
+$plugin->version   = 2026063000;
 $plugin->requires  = 2023042400.00; // Requires Moodle 4.2.
 $plugin->supported = [402, 502];
 $plugin->cron      = 0;
